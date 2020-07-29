98d81b914c78db1b73e9a6c28cc4ea8e5f4ac052
Ben <7553969+benglo@users.noreply.github.com>
2020-07-30 07:56:59 +1000

    Update constants_creator.js

--- constants_creator.js
+++ constants_creator.js
@@ -7,17 +7,17 @@ function camelize(str) {
 }
 
 
-var scope, regEx, tableName, tableGr, tableArray = [], constants = '', scriptGr;
+var scope, regEx, tableName, tableGr, tableArray = [], constants = '', scriptGr, scriptName;
 
 regEx = /(x_qwo15_ct_)/; //Scope name
-scope = '9eae4584dbd69c50cf6a6f76059619a7'
+scope = '9eae4584dbd69c50cf6a6f76059619a7';
+scriptName = 'constants';
 
 tableGr = new GlideRecord('sys_db_object');
 		tableGr.addEncodedQuery('nameLIKEx_qwo15_ct'); //Find all tables that are in the app
 		tableGr.query();
 
 while(tableGr.next()) { //BEGIN THE LOOP!
-  fieldArray = [];
   tableName = tableGr.name.getDisplayValue().replace(regEx, ""); //remove app name
     
   tableArray.push({
@@ -34,16 +34,15 @@ for (var i = 0; i < tableArray.length; i++) {
 }
 
 scriptGr = new GlideRecord('sys_script_include');
-scriptGr.addEncodedQuery('sys_scope=9eae4584dbd69c50cf6a6f76059619a7^name=constants');
+scriptGr.addEncodedQuery('sys_scope='+scope+'^name='+scriptName);
 scriptGr.query();
 
 if(scriptGr.next()) {
 	scriptGr.script = constants;
   scriptGr.update();
 } else {
-scriptGr = new GlideRecord('sys_script_include');
 scriptGr.newRecord();
-scriptGr.name = 'constants';
+scriptGr.name = scriptName;
 scriptGr.script = constants;
 scriptGr.sys_scope = scope;
 scriptGr.insert();
