f92dd8c985e11afaf67686ef1cba550d212b83d5
Ben <7553969+benglo@users.noreply.github.com>
2020-07-30 08:30:10 +1000

    Update constants_creator.js

--- constants_creator.js
+++ constants_creator.js
@@ -1,36 +1,39 @@
 //Constants creator
+var scope, regEx, tableName, tableGr, tableArray = [], constants = '', scriptGr, scriptName, scriptDesc;
 
 function camelize(str) {
-  return str.replace(/(?:^\w|[A-Z]|\b\w)/g, function(word, index) {
-    return index === 0 ? word.toLowerCase() : word.toUpperCase();
-  }).replace(/\s+/g, '');
+	return str.replace(/(?:^\w|[A-Z]|\b\w)/g, function(word, index) {
+		return index === 0 ? word.toLowerCase() : word.toUpperCase();
+	}).replace(/\s+/g, '');
 }
-
-
-var scope, regEx, tableName, tableGr, tableArray = [], constants = '', scriptGr, scriptName;
 
 regEx = /(x_qwo15_ct_)/; //Scope name
+scopeName = 'x_qwo15_ct';
 scope = '9eae4584dbd69c50cf6a6f76059619a7';
 scriptName = 'constants';
+scriptDesc = '';
 
 tableGr = new GlideRecord('sys_db_object');
-		tableGr.addEncodedQuery('nameLIKEx_qwo15_ct'); //Find all tables that are in the app
-		tableGr.query();
+tableGr.addEncodedQuery('nameLIKEx_qwo15_ct'); //Find all tables that are in the app
+tableGr.query();
 
 while(tableGr.next()) { //BEGIN THE LOOP!
-  tableName = tableGr.name.getDisplayValue().replace(regEx, ""); //remove app name
-    
-  tableArray.push({
-    table: tableGr.name.getDisplayValue(),
-    name: tableName,
-    label: tableGr.label.getDisplayValue(),
-    camelCase: camelize(tableGr.label.getDisplayValue())
-		});
+	tableName = tableGr.name.getDisplayValue().replace(regEx, ""); //remove app name
+
+	tableArray.push({
+		table: tableGr.name.getDisplayValue(),
+		name: tableName,
+		label: tableGr.label.getDisplayValue(),
+		camelCase: camelize(tableGr.label.getDisplayValue())
+	});
 }
 
+constants = 'var ' + scriptName + ' = Class.create();' + '\n\n\n';
+constants += "constants.scope = '" + scopeName + "';" + '\n ';
+
 for (var i = 0; i < tableArray.length; i++) {
-  var constant = 'constants.'+ tableArray[i].camelCase +"Table = '" + tableArray[i].table + "';" + '\n ';
-  constants += constant;
+	var constant = 'constants.'+ tableArray[i].camelCase +"Table = '" + tableArray[i].table + "';" + '\n ';
+	constants += constant;
 }
 
 scriptGr = new GlideRecord('sys_script_include');
@@ -39,11 +42,12 @@ scriptGr.query();
 
 if(scriptGr.next()) {
 	scriptGr.script = constants;
-  scriptGr.update();
+	scriptGr.update();
 } else {
-scriptGr.newRecord();
-scriptGr.name = scriptName;
-scriptGr.script = constants;
-scriptGr.sys_scope = scope;
-scriptGr.insert();
+	scriptGr.newRecord();
+	scriptGr.name = scriptName;
+	scriptGr.script = constants;
+	scriptGr.sys_scope += scope;
+	scriptGr.description = scriptDesc;
+	scriptGr.insert();
 }
